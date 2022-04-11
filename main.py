72630104f75c13a8aa027e02843def08d3f20fa3
prachiwhitehat <71639231+prachiwhitehat@users.noreply.github.com>
2022-04-11 14:23:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from flask import Flask, jsonify, request
 
 from storage import all_movies, liked_movies, not_liked_movies, did_not_watch
-from demographic_filtering import output
-from content_filtering import get_recommendations
+
 
 app = Flask(__name__)
 
@@ -48,49 +47,8 @@ def did_not_watch_view():
         "status": "success"
     }), 201
 
-@app.route("/popular-movies")
-def popular_movies():
-    movie_data = []
-    for movie in output:
-        _d = {
-            "title": movie[0],
-            "poster_link": movie[1],
-            "release_date": movie[2] or "N/A",
-            "duration": movie[3],
-            "rating": movie[4],
-            "overview": movie[5]
-        }
-        movie_data.append(_d)
-    return jsonify({
-        "data": movie_data,
-        "status": "success"
-    }), 200
 
-@app.route("/recommended-movies")
-def recommended_movies():
-    all_recommended = []
-    for liked_movie in liked_movies:
-        output = get_recommendations(liked_movie[19])
-        for data in output:
-            all_recommended.append(data)
-    import itertools
-    all_recommended.sort()
-    all_recommended = list(all_recommended for all_recommended,_ in itertools.groupby(all_recommended))
-    movie_data = []
-    for recommended in all_recommended:
-        _d = {
-            "title": recommended[0],
-            "poster_link": recommended[1],
-            "release_date": recommended[2] or "N/A",
-            "duration": recommended[3],
-            "rating": recommended[4],
-            "overview": recommended[5]
-        }
-        movie_data.append(_d)
-    return jsonify({
-        "data": movie_data,
-        "status": "success"
-    }), 200
+
 
 if __name__ == "__main__":
   app.run()

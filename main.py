393066504f9b60429fed3ddbdae70f4ea56d127d
prachiwhitehat <71639231+prachiwhitehat@users.noreply.github.com>
2022-05-09 08:42:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,6 +47,49 @@ def did_not_watch_view():
         "status": "success"
     }), 201
 
+@app.route("/popular-movies")
+def popular_movies():
+    movie_data = []
+    for movie in output:
+        _d = {
+            "title": movie[0],
+            "poster_link": movie[1],
+            "release_date": movie[2] or "N/A",
+            "duration": movie[3],
+            "rating": movie[4],
+            "overview": movie[5]
+        }
+        movie_data.append(_d)
+    return jsonify({
+        "data": movie_data,
+        "status": "success"
+    }), 200
+
+@app.route("/recommended-movies")
+def recommended_movies():
+    all_recommended = []
+    for liked_movie in liked_movies:
+        output = get_recommendations(liked_movie[19])
+        for data in output:
+            all_recommended.append(data)
+    import itertools
+    all_recommended.sort()
+    all_recommended = list(all_recommended for all_recommended,_ in itertools.groupby(all_recommended))
+    movie_data = []
+    for recommended in all_recommended:
+        _d = {
+            "title": recommended[0],
+            "poster_link": recommended[1],
+            "release_date": recommended[2] or "N/A",
+            "duration": recommended[3],
+            "rating": recommended[4],
+            "overview": recommended[5]
+        }
+        movie_data.append(_d)
+    return jsonify({
+        "data": movie_data,
+        "status": "success"
+    }), 200
 
 
 
